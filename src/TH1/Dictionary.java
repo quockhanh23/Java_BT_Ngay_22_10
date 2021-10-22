f6dde79077fba620720efecd1c41159f9e779541
unknown <yeuthuongvoivangdiqua@gmail.com>
2021-10-22 15:23:24 +0700

    bt

---
+++ src/TH1/Dictionary.java
@@ -0,0 +1,51 @@
+package TH1;
+
+import java.util.*;
+
+public class Dictionary {
+    private TreeMap<String, String> dictionary = new TreeMap<>();
+
+    public Dictionary(TreeMap<String, String> dictionary) {
+        this.dictionary = dictionary;
+    }
+
+    public Dictionary() {
+        dictionary.put("cat", "mèo");
+        dictionary.put("dog", "chó");
+        dictionary.put("rat", "chuột");
+        dictionary.put("chicken", "gà");
+        dictionary.put("cockroaches", "gián");
+        dictionary.put("worm", "giun");
+        dictionary.put("duck", "vịt");
+        dictionary.put("goose", "ngỗng");
+        dictionary.put("pig", "heo");
+        dictionary.put("Turkey", "gà tây");
+
+    }
+
+    public TreeMap<String, String> getDictionary() {
+        return dictionary;
+    }
+
+    public void setDictionary(TreeMap<String, String> dictionary) {
+        this.dictionary = dictionary;
+    }
+
+    public void add(String eng, String vie) {
+        this.dictionary.put(eng, vie);
+    }
+
+    public void search(String eng) {
+        if (this.dictionary.containsKey(eng)) {
+            System.out.println(eng + " : " + dictionary.get(eng));
+        } else {
+            System.out.println("can't find " + eng);
+        }
+
+    }
+
+    public void print() {
+        System.out.println(this.dictionary);
+    }
+
+}
---
+++ src/TH1/Main.java
@@ -0,0 +1,11 @@
+package TH1;
+
+public class Main {
+    public static void main(String[] args) {
+        Dictionary dictionary = new Dictionary();
+        dictionary.print();
+        dictionary.search("xxxx");
+        dictionary.search("pig");
+
+    }
+}
